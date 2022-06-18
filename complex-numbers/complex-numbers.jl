61742ea72881262d9f94c4bb4201f1b610fdbc92
Ana Rita Diniz da Cruz <ana.cruz@ee.ufcg.edu.br>
2022-06-18 13:01:25 -0300

    [COMPLEX-NUMBERS] Ajustes de ordem das funções - Permanece passando em todos os testes

--- complex-numbers/complex-numbers.jl
+++ complex-numbers/complex-numbers.jl
@@ -7,20 +7,15 @@ end
 
 ComplexNumber(real::Real, imag::Real) = ComplexNumber(promote(real,imag)...)
 
+abs(c::ComplexNumber) = sqrt(c.real^2+c.imag^2)
+conj(c::ComplexNumber) = ComplexNumber(c.real, - c.imag)
+real(c::ComplexNumber) = c.real 
+imag(c::ComplexNumber) = c.imag 
+
 +(c1::ComplexNumber, c2::ComplexNumber) = ComplexNumber(c1.real + c2.real, c1.imag + c2.imag)
-
 -(c1::ComplexNumber, c2::ComplexNumber) = ComplexNumber(c1.real - c2.real, c1.imag - c2.imag)
-
 *(c1::ComplexNumber, c2::ComplexNumber) = ComplexNumber(c1.real*c2.real - c1.imag*c2.imag, c1.real*c2.imag + c1.imag*c2.real)
-
-/(c1::ComplexNumber, c2::ComplexNumber) = ComplexNumber(Float64(c1.real * c2.real + c1.imag * c2.imag)/(c2.real^2 + c2.imag^2) , Float64(c1.imag * c2.real - c1.real * c2.imag)/(c2.real^2 + c2.imag^2))
-
+/(c1::ComplexNumber, c2::ComplexNumber) = ComplexNumber(Float64(c1.real * c2.real + c1.imag * c2.imag)/abs(c2)^2 , Float64(c1.imag * c2.real - c1.real * c2.imag)/abs(c2)^2)
 ≈(c1::ComplexNumber, c2::ComplexNumber) = isapprox(c1.real, c2.real) && isapprox(c1.imag, c2.imag)
 
-abs(c::ComplexNumber) = sqrt(c.real^2+c.imag^2)
 
-conj(c::ComplexNumber) = ComplexNumber(c.real, - c.imag)
-
-real(c::ComplexNumber) = c.real 
-
-imag(c::ComplexNumber) = c.imag 
