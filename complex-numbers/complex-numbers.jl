721577031bcda71fee533fc63f11d4ec4a1f61a8
Ana Rita Diniz da Cruz <ana.cruz@ee.ufcg.edu.br>
2022-06-18 12:20:33 -0300

    [COMPLEX-NUMBERS] Implementando '-' para ComplexNumber sem sobrepor o já exixtente - Passou no segundo set do teste 'Arithmetic'

--- complex-numbers/complex-numbers.jl
+++ complex-numbers/complex-numbers.jl
@@ -1,4 +1,4 @@
-import Base: +
+import Base: +, -
 
 struct ComplexNumber{T<:Real} <: Number
     real::T
@@ -8,3 +8,5 @@ end
 ComplexNumber(real::Real, imag::Real) = ComplexNumber(promote(real,imag)...)
 
 +(c1::ComplexNumber, c2::ComplexNumber) = ComplexNumber(c1.real + c2.real, c1.imag + c2.imag)
+
+-(c1::ComplexNumber, c2::ComplexNumber) = ComplexNumber(c1.real - c2.real, c1.imag - c2.imag)
