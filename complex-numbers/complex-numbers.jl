0c4ed07f800983807cb1dce6c45c4b4e0ee2256d
Ana Rita Diniz da Cruz <ana.cruz@ee.ufcg.edu.br>
2022-06-18 12:10:40 -0300

    [COMPLEX-NUMBERS] Definição de Parâmetro que seja real e construtor externo para fazer 'promote' caso os tipos dos atributos sejam diferentes

--- complex-numbers/complex-numbers.jl
+++ complex-numbers/complex-numbers.jl
@@ -1,5 +1,6 @@
-struct ComplexNumber{T} <: Number
-    
+struct ComplexNumber{T<:Real} <: Number
+    real::T
+    imag::T
 end
 
-
+ComplexNumber(real::Real, imag::Real) = ComplexNumber(promote(real,imag)...)
